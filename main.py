c13ee842341012a3c206d5e3cfcb333e24b26227
Dener Chaves <denerdeveloper26@gmail.com>
2026-06-04 18:58:40 -0300

    v1-projeto

---
+++ main.py
@@ -0,0 +1,29 @@
+#cv2 é a abreviação de OpenCV, uma biblioteca de visão computacional que permite ler e processar imagens. Pytesseract é uma biblioteca Python que serve como um wrapper para o Tesseract OCR, permitindo extrair texto de imagens usando OCR (Optical Character Recognition).
+import cv2 
+import pytesseract
+
+
+
+#Ler imgagem
+imagem = cv2.imread('comprovante_pix.jpeg')
+
+#Estamos definindo o caminho do tesseract para o pytesseract, para que ele possa usar o OCR para extrair texto da imagem.
+caminho = r"C:\Users\dener\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+
+pytesseract.pytesseract.tesseract_cmd = caminho
+#Pedir pytessact Estrair texto da imgagem 
+texto = pytesseract.image_to_string(imagem)
+
+#colocar cada linha dentro de uma posição de uma listagit
+linhas = texto.split('\n')
+
+for linha in linhas:
+    if 'R$' in linha:
+        #Verificar valor do pix
+        #print('Valor do Pix encontrado:', linha)
+        #Separando a string ultilizando o separador R$ e atribuindo a posição 1, que contem o valor do pix. 
+        VALOR_PIX = linha.split('R$')[1]
+
+print('Valor do Pix encontrado:', VALOR_PIX)
+
+print('lista:', linhas)
