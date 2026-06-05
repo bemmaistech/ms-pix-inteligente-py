17ab2f430f5015605e4c4e7b5baf0fb5cca96749
Dener Chaves <denerdeveloper26@gmail.com>
2026-06-05 18:07:15 -0300

    Remove Python bytecode files and add .gitignore

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from trataentoPng import ImageOCR
+from tramentoDados import TextoTratamento
 
 
 imagem = "comprovanteNubank.jpeg"  # Ajuste para o caminho da sua imagem
-tratamento = ImageOCR()
-texto = tratamento.OCR(imagem)
+tratamento_ocr = ImageOCR()
+texto = tratamento_ocr.OCR(imagem)
+
+tratamento_texto = TextoTratamento(texto)
+linhas_tratadas = tratamento_texto.process()
 
 print("\n=== TEXTO EXTRAÍDO ===\n")
 print(texto)
+print("\n=== LINHAS TRATADAS ===\n")
+print(linhas_tratadas)
 
